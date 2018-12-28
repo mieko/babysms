dae99c71e1c4ced8c5246019d7cde810e43933fe
patrickclery <patrick.clery@gmail.com>
2018-12-28 04:53:14 -0800

    implementing "Strategy" pattern

--- lib/babysms/models/message.rb
+++ lib/babysms/models/message.rb
@@ -3,24 +3,18 @@ require "phony_rails"
 
 module BabySMS
   class Message
+    ###########################################################################
     include ActiveModel::Model
     include ActiveModel::Validations::Callbacks
 
+    ###########################################################################
     attr_accessor :recipient, :recipient_as_normalized
     phony_normalize :recipient, default_country_code: 'US'
-    validates :recipient, presence: true,
-                          phony_plausible: true
+    validates :recipient, presence: true, phony_plausible: true
 
+    ###########################################################################
     attr_accessor :contents
-
     # 1600 is the safe limit for most handsets to stitch together messages.
-    validates :contents, presence: true,
-                         length: { maximum: 1600 }
-
-    def deliver_now
-      raise_validation_error unless valid?
-
-      BabySMS.adapter.deliver_now(self)
-    end
+    validates :contents, presence: true, length: { maximum: 1600 }
   end
 end
--- lib/babysms/models/strategy.rb
+++ lib/babysms/models/strategy.rb
@@ -2,43 +2,25 @@ require 'active_support/callbacks'
 require 'rainbow/refinement'
 
 module BabySMS
-  class Strategy
+  module Strategy
 
     include ActiveModel::Model
     include ActiveSupport::Concern
-    include ActiveSupport::Callbacks
 
-    set_callbacks
+    delegate :send_message, to: :strategy, allow_nil: false
+
+    validates :strategy, presence: true
 
     protected
 
-    attr_accessor :messages
     attr_accessor :strategy
     attr_accessor :verbose
 
-    delegate :call, to: :strategy, allow_nil: false
-
     public
 
-    validates :messages, presence: true
-    validates :strategy, presence: true
-
-    def initialize(messages:, verbose: true, strategy:,)
-      @messages = messages || []
+    def initialize(verbose: true, strategy:,)
       @verbose  = verbose
       @strategy = strategy
-    end
-
-    def call
-      fail 'Missing implementation'
-    end
-
-    def strategy=(strategy)
-      super
-
-      set_callback :send_message, :after do |object|
-        # Some shit
-      end
     end
 
   end
